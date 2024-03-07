b403069f026bc8a51eb511d7921e398231121443
tor <torberge@outlook.com>
2024-03-07 22:26:01 +0100

    wip

--- flake.nix
+++ flake.nix
@@ -1,24 +1,34 @@
 {
-  description = "a flakey flake";
+  # Your existing flake.nix content
+  outputs = { self, nixpkgs, home-manager, ... }: {
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs, ... }: {
     nixosConfigurations.mySystem = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./nixos/configuration.nix
+        home-manager.nixosModules.home-manager
         ({ pkgs, ... }: {
           environment.systemPackages = with pkgs; [
-            (pkgs.writeShellScriptBin "rebuild-flake" ''
-              #!/usr/bin/env bash
-              sudo nixos-rebuild switch --flake './flake#mySystem'
-            '')
+            # Your existing packages
           ];
+
+          # Modified Home Manager users configuration
+          home-manager.users.tor = { pkgs, ... }: {
+            home.stateVersion = "22.05"; # Ensure this matches your NixOS state version
+
+            home.file.".config" = {
+              source = builtins.path { 
+                path = ./dotfiles/.config;
+                name = "dotfiles-config";
+              };
+              recursive = true;
+            };
+
+            # Your existing home-manager configurations
+          };
         })
       ];
     };
   };
 }
+
