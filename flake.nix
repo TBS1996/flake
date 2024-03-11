cb521560431408a2a710f9184980f5906b70404a
Tor <torberge@outlook.com>
2024-03-11 14:45:25 +0100

    wip

--- flake.nix
+++ flake.nix
@@ -48,7 +48,7 @@
 
 		# Privacy Badger:
 		"446900e4-71c2-419f-a6a7-df9c091e268b" = {
-		  install_url = "https://addons.mozilla.org/firefox/downloads/latest/df-youtube/latest.xpi";
+		  install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
 		  installation_mode = "force_installed";
 	  	  };
 		
