dc6b4e67858618418090419b44f7d88a37bec474
Tor <torberge@outlook.com>
2024-03-11 14:39:55 +0100

    wip

--- flake.nix
+++ flake.nix
@@ -32,15 +32,21 @@
 	      enable = true;
 	      policies = {
 	      ExtensionSettings = {
-		"*".installation_mode = "blocked"; # blocks all addons except the ones specified below
+		#"*".installation_mode = "blocked"; # blocks all addons except the ones specified below
 
 		# uBlock Origin:
 		"uBlock0@raymondhill.net" = {
 		  install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
 		  installation_mode = "force_installed";
-		};
+		  };
+
+		# Privacy Badger:
+		"jid1-MnnxcxisBPnSXQ@jetpack" = {
+		  install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+		  installation_mode = "force_installed";
+	  	  };
 		
-	      };
+	        };
 
 	      };
 
