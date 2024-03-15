2467a6d124f4df5da2a33936823fad96965f8e1e
Tor <torberge@outlook.com>
2024-03-15 15:36:45 +0100

    rebuilding

--- systemd-services.nix
+++ systemd-services.nix
@@ -8,7 +8,7 @@
       ExecStart = "/home/tor/flake/scripts/write_date.sh";
       Type = "oneshot";
       User = "tor"; # Ensure this is the correct user
-      Environment = "PATH=${lib.makeBinPath [ pkgs.git pkgs.coreutils ]}";
+      Environment = "PATH=${pkgs.lib.makeBinPath [ pkgs.git pkgs.coreutils ]}";
     };
   };
 
