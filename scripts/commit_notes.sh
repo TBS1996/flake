bf912fa07ce37b45b9757bcf2983684988363d1e
Tor <torberge@outlook.com>
2024-03-15 14:08:39 +0100

    rebuilding

--- scripts/commit_notes.sh
+++ scripts/commit_notes.sh
@@ -1,10 +1,7 @@
 #! /nix/store/xl6mxfpj2869xbr6l6j060yay55rscd9-system-path/bin/bash
 
-exec > /tmp/commitNotes.log 2>&1
-set -x
 
-# Your script's contents here
-
+nix-shell -p git
 
 # Navigate to your notes directory
 cd /home/tor/velv/
