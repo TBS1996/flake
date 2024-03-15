df2605c9df46c6b6e8eef51a9e228fd1622fa350
Tor <torberge@outlook.com>
2024-03-15 15:28:59 +0100

    rebuilding

--- scripts/write_date.sh
+++ scripts/write_date.sh
@@ -3,4 +3,6 @@ echo "hey the current date and time: $(date)" >> /home/tor/date_log.txt
 
 cd /home/tor/velv
 /run/current-system/sw/bin/git add .
+/run/current-system/sw/bin/git commit -m 'autocommit'
+/run/current-system/sw/bin/git push
 
