abe80b6cd1e72a5e796bb76048cc18b218512d7f
Tor <torberge@outlook.com>
2024-03-15 14:13:31 +0100

    rebuilding

--- systemd-services.nix
+++ systemd-services.nix
@@ -4,18 +4,18 @@ let
   commitNotesScript = pkgs.writeShellScriptBin "commit_notes" (builtins.readFile ./scripts/commit_notes.sh);
 in
 {
-
-systemd.services.commitNotes = {
-  description = "Commit and push notes periodically";
-  wantedBy = [ "multi-user.target" ];
-  after = [ "network.target" ];
-  serviceConfig = {
-    ExecStart = "/home/tor/flake/scripts/commit_notes.sh";
-    Type = "oneshot";
-    RemainAfterExit = true;
+  systemd.services.commitNotes = {
+    description = "Commit and push notes periodically";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+    serviceConfig = {
+      ExecStart = "/home/tor/flake/scripts/commit_notes.sh";
+      Type = "oneshot";
+      RemainAfterExit = true;
+      User = "tor"; # Run the service as user 'tor'
+      Group = "tor"; # Run the service with the group 'tor'
+    };
   };
-};
-
 
   systemd.timers.commitNotesTimer = {
     description = "Timer for commitNotes service";
