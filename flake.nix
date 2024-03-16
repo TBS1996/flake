eb427812f7204d30a80b9bca09126f008e574af5
Tor <torberge@outlook.com>
2024-03-17 00:36:10 +0100

    wip

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       modules = [
         ./nixos/configuration.nix
 	./systemd-services.nix
-	./nixvim-config.nix
+	#./nixvim-config.nix
         home-manager.nixosModules.home-manager
         ({ pkgs, ... }: {
 
@@ -26,6 +26,8 @@
 
             imports = [
               nixvim.homeManagerModules.nixvim
+	      ./options/nixvim-config.nix
+	      ./options/firefox-config.nix
             ];
 
             programs.git = {
@@ -36,41 +38,6 @@
 
 	    programs.neomutt = {
 	      enable = true;
-	    };
-
-	    programs.firefox = {
-	      enable = true;
-	      policies = {
-	      ExtensionSettings = {
-		# uBlock Origin:
-		"uBlock0@raymondhill.net" = {
-		  install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-		  installation_mode = "force_installed";
-		  };
-
-		# Privacy Badger:
-		"jid1-MnnxcxisBPnSXQ@jetpack" = {
-		  install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
-		  installation_mode = "force_installed";
-	  	  };
-
-		# bitwarden
-		"{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-		  install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
-		  installation_mode = "force_installed";
-	  	  };
-
-
-		# df youtube
-		"dfyoutube@example.com" = {
-		  install_url = "https://addons.mozilla.org/firefox/downloads/latest/df-youtube/latest.xpi";
-		  installation_mode = "force_installed";
-	  	  };
-		
-	        };
-
-	      };
-
 	    };
 
 	    programs.waybar.enable = true;
---
+++ options/firefox-config.nix
@@ -0,0 +1,43 @@
+{pkgs, ...}:
+
+{
+
+
+ programs.firefox = {
+   enable = true;
+   policies = {
+   ExtensionSettings = {
+      # uBlock Origin:
+      "uBlock0@raymondhill.net" = {
+       install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+       installation_mode = "force_installed";
+       };
+
+      # Privacy Badger:
+      "jid1-MnnxcxisBPnSXQ@jetpack" = {
+       install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+       installation_mode = "force_installed";
+       };
+
+      # bitwarden
+      "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+       install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+       installation_mode = "force_installed";
+       };
+
+
+      # df youtube
+      "dfyoutube@example.com" = {
+       install_url = "https://addons.mozilla.org/firefox/downloads/latest/df-youtube/latest.xpi";
+       installation_mode = "force_installed";
+       };
+      
+     };
+
+   };
+
+ };
+
+
+
+}
