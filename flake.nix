59287bd6657261d6bd09725690f94517b9243534
Tor <torberge@outlook.com>
2024-03-16 14:08:14 +0100

    rebuilding

--- flake.nix
+++ flake.nix
@@ -108,23 +108,48 @@
                 " Any additional custom Vimscript
               '';
 
-              extraConfigLua = ''
+
+	      extraConfigLua = ''
 		vim.g.mapleader = " "
 		vim.api.nvim_set_keymap('n', '<leader>lg', ':Telescope live_grep<CR>', { noremap = true, silent = true })
 		vim.api.nvim_set_keymap('n', '<leader>gs', ':Telescope grep_string<CR>', { noremap = true, silent = true })
 
-                require('rust-tools').setup({})
-                require('lspconfig').rust_analyzer.setup({
-                  capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-                })
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                  pattern = {"*.rs"},
-                  callback = function()
-                    vim.lsp.buf.format({ timeout_ms = 1000, async = false })
-                  end,
-                })
-                -- Additional Lua configuration for LSP, cmp, treesitter, etc.
-              '';
+		-- Define an 'on_attach' function to set up key mappings when an LSP client attaches to a buffer
+		local on_attach = function(client, bufnr)
+		  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+		  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+		  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+		  -- Key mappings
+		  local opts = { noremap=true, silent=true }
+		  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+		  -- Add more mappings as needed
+		end
+
+		-- Setup 'rust-tools'
+		require('rust-tools').setup({
+		  -- Assuming 'rust-tools' takes an 'on_attach' argument in its setup
+		  server = {
+		    on_attach = on_attach,
+		    capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities()),
+		  },
+		})
+
+		-- Setup for other LSP servers can also include the 'on_attach' function
+		require('lspconfig').rust_analyzer.setup({
+		  on_attach = on_attach,
+		  capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities()),
+		})
+
+		vim.api.nvim_create_autocmd("BufWritePre", {
+		  pattern = {"*.rs"},
+		  callback = function()
+		    vim.lsp.buf.format({ timeout_ms = 1000, async = false })
+		  end,
+		})
+		-- Additional Lua configuration for LSP, cmp, treesitter, etc.
+	      '';
             };
 
 
