531f6fde1549e362f6884d1942736838b9ee1660
Tor <torberge@outlook.com>
2024-03-18 00:38:54 +0100

    rebuilding

--- options/nixvim-config.nix
+++ options/nixvim-config.nix
@@ -67,7 +67,7 @@
 
 
       -- Define an 'on_attach' function to set up key mappings when an LSP client attaches to a buffer
-lcal on_attach = function(client, bufnr)
+        local on_attach = function(client, bufnr)
         local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
         local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
