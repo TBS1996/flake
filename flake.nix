8b8fd18aaed11aaa4d9dc2000bcdb2b2b34de6b7
Tor <torberge@outlook.com>
2024-04-26 20:23:57 +0200

    rebuilding

--- flake.nix
+++ flake.nix
@@ -34,6 +34,24 @@
 	      enable = true;
 	    };
 
+	    services.mpd = {
+	      enable = true;
+	      musicDirectory = "/path/to/music";
+	      extraConfig = ''
+		# must specify one or more outputs in order to play audio!
+		# (e.g. ALSA, PulseAudio, PipeWire), see next sections
+		audio_output {
+		  type "pulse"
+		  name "My PulseAudio" # this can be whatever you want
+		}
+	      '';
+
+	      # Optional:
+	      network.listenAddress = "any"; # if you want to allow non-localhost connections
+	      startWhenNeeded = true; # systemd feature: only start MPD service upon connection to its socket
+	  };
+
+
             programs.git = {
               enable = true;
               userName = "tor";
