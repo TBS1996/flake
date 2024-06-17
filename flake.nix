32134d1a259be9b2a438ad95fd34348dd76e0cbd
Tor <torberge@outlook.com>
2024-06-17 20:41:28 +0200

    rebuilding

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     nixvim.url = "github:nix-community/nixvim";
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
