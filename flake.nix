414682a172236f9a3b21ba54f11bd169cbb696ce
Tor <torberge@outlook.com>
2024-08-09 13:00:33 +0200

    rebuilding

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager.url = "github:nix-community/home-manager";
-    nixvim.url = "github:nix-community/nixvim";
-    nixvim.inputs.nixpkgs.follows = "nixpkgs";
+   # nixvim.url = "github:nix-community/nixvim";
+  #  nixvim.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, home-manager, nixvim, ... }: {
@@ -24,8 +24,8 @@
             };
 
             imports = [
-              nixvim.homeManagerModules.nixvim
-              ./options/nixvim-config.nix
+            # nixvim.homeManagerModules.nixvim
+            #  ./options/nixvim-config.nix
               ./options/firefox-config.nix
             ];
 
