7f95766623283bd95f35b3e0c2b42bbcab53f45c
Tor <torberge@outlook.com>
2024-08-23 11:27:27 +0200

    rebuilding

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -3,3 +3,6 @@ require('plugins')
 require('keymaps')
 require('rust')  -- Add this line to load the Rust configuration
 
+vim.cmd('syntax enable')            -- Enable syntax highlighting
+vim.o.background = "dark"           -- Use dark background (or "light" for a light version)
+vim.cmd('colorscheme gruvbox')      -- Set the color scheme to Gruvbox
