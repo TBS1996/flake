352bcffbcd9f643d3ff54bcf1e7e7018bab2742e
Tor <torberge@outlook.com>
2024-10-13 19:17:11 +0200

    Add hardware-configuration for nixos

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -8,21 +8,22 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "vmd" "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/c6c4625f-a224-4e03-99d1-c442fe43015f";
+    { device = "/dev/disk/by-uuid/8fadc4b5-271c-46d6-8005-07f3b8ee830e";
       fsType = "ext4";
     };
 
-  boot.initrd.luks.devices."luks-f9ee91df-95d6-4b44-95c8-fad35d0f60ca".device = "/dev/disk/by-uuid/f9ee91df-95d6-4b44-95c8-fad35d0f60ca";
+  boot.initrd.luks.devices."luks-e4ed8064-3f60-4b2e-b76f-9fbf9fc621f8".device = "/dev/disk/by-uuid/e4ed8064-3f60-4b2e-b76f-9fbf9fc621f8";
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/F605-6054";
+    { device = "/dev/disk/by-uuid/F106-9B01";
       fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices = [ ];
@@ -32,7 +33,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
