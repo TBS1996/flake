3097d1504f98ca63b03c59817d0172f4164e0adc
Tor <torberge@outlook.com>
2024-10-14 21:42:27 +0200

    rebuilding

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -55,12 +55,9 @@
 
 hardware = {
     opengl = {
-      driSupport = true;
-      driSupport32Bit = true;
 
       ## amdvlk: an open-source Vulkan driver from AMD
       extraPackages = [ pkgs.amdvlk ];
-      extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
     };
     pulseaudio.enable = false;  
 };
