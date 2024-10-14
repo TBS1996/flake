77a9348e92fbdad09e7d2bb340a7633d3a6c1331
Tor <torberge@outlook.com>
2024-10-14 21:49:01 +0200

    rebuilding

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -1,73 +1,73 @@
 { pkgs }:
 
 with pkgs; [
-firefox
-    tdesktop
-    whatsapp-for-linux
-    tor-browser
-    zathura
-    calibre
-    element-desktop
-    kdenlive
-    obs-studio
-    gimp
-    xfce.thunar
-    android-studio
-    vscode
-    neovim
-    steam
-    prismlauncher
-    jdk8
-    atlauncher
-    zed-editor
-    
-    # cli
-    alacritty
-    ripgrep
-    tree
-    mpd
-    ncmpcpp
-    python3
-    feh
-    cron
-    wdisplays
-    mpv
-    fastfetch
-    btop
-    bat
-    bashmount
-    ncdu
-    fzf
-    newsboat
-    github-cli
-    dmenu
-    git
-    acpi
-    cargo
-    pamixer
-    aerc
-    yazi
-    wl-clipboard
-    fish
-    syncthing
-    zip
-    brightnessctl
-
-    # bg
-    rustup
-    rust-analyzer
-    rustfmt
-    lldb
-    swaylock
-    xdg-utils
-    openssl
-    pkg-config
-    rustc
-    gcc
-    slurp
-    vulkan-loader
-    vulkan-tools
-    mesa # Mesa provides Vulkan for Intel and AMD
+  # GUI apps
+  firefox
+  tdesktop
+  whatsapp-for-linux
+  tor-browser
+  zathura
+  calibre
+  element-desktop
+  kdenlive
+  obs-studio
+  gimp
+  xfce.thunar
+  android-studio
+  vscode
+  neovim
+  steam
+  prismlauncher
+  jdk8
+  atlauncher
+  zed-editor
+  
+  # CLI apps
+  alacritty
+  ripgrep
+  tree
+  mpd
+  ncmpcpp
+  python3
+  feh
+  cron
+  wdisplays
+  mpv
+  fastfetch
+  btop
+  bat
+  bashmount
+  ncdu
+  fzf
+  newsboat
+  github-cli
+  dmenu
+  git
+  acpi
+  cargo
+  pamixer
+  aerc
+  yazi
+  wl-clipboard
+  fish
+  syncthing
+  zip
+  brightnessctl
+  
+  # Background utilities
+  rustup
+  rust-analyzer
+  rustfmt
+  lldb
+  swaylock
+  xdg-utils
+  openssl
+  pkg-config
+  rustc
+  gcc
+  slurp
+  vulkan-loader  # Make sure this is available in the current channel
+  vulkan-tools
+  mesa
 ]
-
 
