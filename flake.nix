0e3a0cfd123314909c238b921dfb2b6dc85568c4
Tor <torberge@outlook.com>
2025-02-04 17:38:18 +0100

    wip

--- flake.nix
+++ flake.nix
@@ -48,10 +48,6 @@
             home.file.".config/zsh/.zshrc".source = ./dotfiles/zsh/config;
             home.file.".config/aerc/binds.conf".source = ./dotfiles/aerc/binds.conf;
             home.file.".config/aerc/aerc.conf".source = ./dotfiles/aerc/aerc.conf;
-           # home.file.".config/nvim/init.lua".source = ./dotfiles/nvim/init.lua;
-           # home.file.".config/nvim/lua/keymaps.lua".source = ./dotfiles/nvim/keymaps.lua;
-           # home.file.".config/nvim/lua/plugins.lua".source = ./dotfiles/nvim/plugins.lua;
-           # home.file.".config/nvim/lua/rust.lua".source = ./dotfiles/nvim/rust.lua;
           };
         })
       ];
