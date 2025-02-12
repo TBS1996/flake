d6fa67ba1aae15f5240373452827b5f5c6b82602
Tor <torberge@outlook.com>
2025-02-12 09:08:25 +0100

    rebuilding

--- flake.nix
+++ flake.nix
@@ -1,7 +1,8 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    home-manager.url = "github:nix-community/home-manager";
+      home-manager.url = "github:nix-community/home-manager/release-24.11"; 
+
   };
 
   outputs = { self, nixpkgs, home-manager, ... }: {
