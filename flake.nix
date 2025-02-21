e4424fedb4178b50cd4b1115c35b7b38385b5c35
Tor <torberge@outlook.com>
2025-02-21 08:56:22 +0100

    rebuilding

--- flake.nix
+++ flake.nix
@@ -51,6 +51,7 @@
             home.file.".config/zsh/.zshrc".source = ./dotfiles/zsh/config;
             home.file.".config/aerc/binds.conf".source = ./dotfiles/aerc/binds.conf;
             home.file.".config/aerc/aerc.conf".source = ./dotfiles/aerc/aerc.conf;
+            home.file.".config/waybar/config".source = ./dotfiles/waybar/config;
           };
         })
       ];
