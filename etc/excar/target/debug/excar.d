270708af44d53fbec481abb1ee3ac497f532cf04
Tor <torberge@outlook.com>
2025-02-23 10:25:47 +0100

    wip

--- etc/excar/target/debug/excar.d
+++
@@ -1,1 +0,0 @@
-/home/tor/flake/etc/excar/target/debug/excar: /home/tor/flake/etc/excar/src/main.rs
--- etc/libcar/target/debug/deps/libcar-81871b559039292c.d
+++
@@ -1,5 +0,0 @@
-/home/tor/flake/etc/libcar/target/debug/deps/libcar-81871b559039292c: src/lib.rs
-
-/home/tor/flake/etc/libcar/target/debug/deps/libcar-81871b559039292c.d: src/lib.rs
-
-src/lib.rs:
