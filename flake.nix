0fa28368f56ea70e2ac41c8c9f2bb0aa36bdb9c2
Tor <torberge@outlook.com>
2025-04-01 12:28:52 +0200

    rebuilding

--- flake.nix
+++ flake.nix
@@ -1,15 +1,15 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/master"; 
+    home-manager.url = "github:nix-community/home-manager/master";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: 
-    let
-      vars = import ./vars.nix;
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let vars = import ./vars.nix;
     in {
       nixosConfigurations.sys = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
+        formatter = with pkgs; [ alejandra ];
         modules = [
           ./nixos/configuration.nix
           ./systemd-services.nix
@@ -17,69 +17,67 @@
           ({ pkgs, ... }: {
             nixpkgs.config.allowUnfree = true;
 
-            imports = [
-              ./options/firefox-config.nix
-            ];
+            imports = [ ./options/firefox-config.nix ];
 
             environment.systemPackages = with pkgs; [ gammastep ];
 
             home-manager.users.tor = { pkgs, ... }: {
-	      imports = [
-		    ./options/helix-config.nix
-	  ];
+              imports = [ ./options/helix-config.nix ];
               home.stateVersion = "24.11";
 
-              services.syncthing = {
-                enable = true;
-              };
+              services.syncthing = { enable = true; };
 
               programs.git = {
                 enable = true;
-                userName = vars.git_name;   
+                userName = vars.git_name;
                 userEmail = vars.git_email;
               };
 
-	      services.mako = {
-		  enable = true;
-		  anchor = "top-left";
-		  defaultTimeout = 5000;
-		  backgroundColor = "#1e1e2e";
-		  textColor = "#cdd6f4";
-		  borderColor = "#89b4fa";
-		  borderSize = 2;
-		  padding = "10";
-		  font = "JetBrainsMono 10";
-		};
+              services.mako = {
+                enable = true;
+                anchor = "top-left";
+                defaultTimeout = 5000;
+                backgroundColor = "#1e1e2e";
+                textColor = "#cdd6f4";
+                borderColor = "#89b4fa";
+                borderSize = 2;
+                padding = "10";
+                font = "JetBrainsMono 10";
+              };
 
-		  services.git-sync = {
-		    enable = true;
-		    repositories = {
-		      infrastructure = {
-			path = "/home/tor/prog/infrastructure";
-			uri = "https://github.com/cognitedata/infrastructure.git";
-			interval = 0; 
-		      };
-		      speki = {
-			path = "/home/tor/prog/speki";
-			uri = "https://github.com/tbs1996/speki.git";
-			interval = 0; 
-		      };
-		      talecast = {
-			path = "/home/tor/prog/talecast";
-			uri = "https://github.com/tbs1996/talecast.git";
-			interval = 0; 
-		      };
-		    };
-		  };
+              services.git-sync = {
+                enable = true;
+                repositories = {
+                  infrastructure = {
+                    path = "/home/tor/prog/infrastructure";
+                    uri = "https://github.com/cognitedata/infrastructure.git";
+                    interval = 0;
+                  };
+                  speki = {
+                    path = "/home/tor/prog/speki";
+                    uri = "https://github.com/tbs1996/speki.git";
+                    interval = 0;
+                  };
+                  talecast = {
+                    path = "/home/tor/prog/talecast";
+                    uri = "https://github.com/tbs1996/talecast.git";
+                    interval = 0;
+                  };
+                };
+              };
 
-              home.file.".config/newsboat/config".source = ./dotfiles/newsboat/config;
-              home.file.".config/newsboat/urls".source = ./dotfiles/newsboat/urls;
+              home.file.".config/newsboat/config".source =
+                ./dotfiles/newsboat/config;
+              home.file.".config/newsboat/urls".source =
+                ./dotfiles/newsboat/urls;
               home.file.".config/foot/foot.ini".source = ./dotfiles/foot/config;
               home.file.".config/sway/config".source = ./dotfiles/sway/config;
               home.file.".config/zsh/.zshrc".source = ./dotfiles/zsh/config;
-              home.file.".config/waybar/config".source = ./dotfiles/waybar/config;
+              home.file.".config/waybar/config".source =
+                ./dotfiles/waybar/config;
               home.file.".cargo/config.toml".source = ./dotfiles/cargo/config;
-              home.file.".config/Code/User/settings.json".source = ./dotfiles/vscode/settings.json;
+              home.file.".config/Code/User/settings.json".source =
+                ./dotfiles/vscode/settings.json;
             };
           })
         ];
