fdec889885ff22a4621073fa0d6d56ce4b995779
Tor <torberge@outlook.com>
2025-07-27 10:53:02 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -123,6 +123,29 @@
                 end,
               })
 
+              -- Set up completion engine (cmp)
+              local cmp = require("cmp")
+              cmp.setup({
+                snippet = {
+                  expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                  end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                  ['<C-Space>'] = cmp.mapping.complete(),
+                  ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                  ['<Tab>'] = cmp.mapping.select_next_item(),
+                  ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+                }),
+                sources = cmp.config.sources({
+                  { name = 'nvim_lsp' },
+                  { name = 'luasnip' },
+                  { name = 'buffer' },
+                  { name = 'path' },
+                })
+              })
+
+              -- Rust LSP config
               require("lspconfig").rust_analyzer.setup({
                 settings = {
                   ["rust-analyzer"] = {
