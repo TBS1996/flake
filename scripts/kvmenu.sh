e8f5c223a9608039b3497b61e01ace3813b58a65
Tor <torberge@outlook.com>
2025-08-07 14:46:58 +0200

    wip

--- scripts/kvmenu.sh
+++ scripts/kvmenu.sh
@@ -3,10 +3,9 @@
 FILE="$HOME/flake/kvstore"
 CLIP_CMD="wl-copy"
 
-entry=$(fzf < "$FILE") || exit 1
-value=$(echo "$entry" | cut -d'=' -f2-)
+entry=$(cut -d'=' -f1 "$FILE" | fzf --layout=reverse) || exit 1
+value=$(grep "^$entry=" "$FILE" | cut -d'=' -f2-)
 
 echo -n "$value" | $CLIP_CMD
 
 notify-send "Copied to clipboard" "$entry"
-
